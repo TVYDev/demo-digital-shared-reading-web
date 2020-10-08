ff481c231df113660225dd5546b25b7a406b67d4
Tang Vannyou <vannyoutang23@gmail.com>
2020-10-08 16:49:34 +0700

    Revert "testtest"

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,10 +9,7 @@
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <style>
         .footer {
-            position: absolute;
-            left: 0;
-            bottom: 15px;
-            /*margin-top: 25px;*/
+            margin-top: 10px;
             text-align: center;
             width: 100%;
             color: white;
