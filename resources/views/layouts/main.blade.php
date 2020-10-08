e7ddd29928361bfd918d081df36677d8d6c4a35c
Tang Vannyou <vannyoutang23@gmail.com>
2020-10-08 16:55:00 +0700

    Revert "Revert "testtest""
    This reverts commit ff481c231df113660225dd5546b25b7a406b67d4.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,7 +9,10 @@
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <style>
         .footer {
-            margin-top: 10px;
+            position: absolute;
+            left: 0;
+            bottom: 15px;
+            /*margin-top: 25px;*/
             text-align: center;
             width: 100%;
             color: white;
